e3c2fede87ab8cef442330d9ae37fa47c829420e
smbrown22 <smbrown22@my.waketech.edu>
2026-04-23 07:47:14 -0400

    created stubs for functions, added first failing test

---
+++ garage.py
@@ -0,0 +1,11 @@
+def enter_garage(garage, car_id, entry_hour):
+    pass 
+
+def exit_garage(garage, car_id):
+    pass
+
+def get_available_spots(garage):
+    pass 
+
+def calculate_fee(hours, rate):
+    pass 
