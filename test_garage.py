e1cbc3f12ee4bcca31062e7f584cd2a592722b8e
smbrown22 <smbrown22@my.waketech.edu>
2026-04-23 07:58:47 -0400

    added test_enter_garage_full_capacity

--- test_garage.py
+++ test_garage.py
@@ -5,3 +5,7 @@ def test_enter_garage_successful_add():
 
     enter_garage(garage, "12345", 8) 
     assert "12345" in garage 
+
+def test_enter_garage_full_capacity():
+    with pytest.raises(ValueError):
+        enter_garage(garage, "12345", 8)
