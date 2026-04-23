8a22ceac42aee7143dfdb94c8a2a73c13f51a9a6
smbrown22 <smbrown22@my.waketech.edu>
2026-04-23 08:00:44 -0400

    created test_enter_garage_car_exists

--- test_garage.py
+++ test_garage.py
@@ -2,7 +2,6 @@ import pytest
 from garage import garage, enter_garage 
 
 def test_enter_garage_successful_add():
-
     enter_garage(garage, "12345", 8) 
     assert "12345" in garage 
 
@@ -10,3 +9,8 @@ def test_enter_garage_full_capacity():
     with pytest.raises(ValueError):
         enter_garage(garage, "54321", 10)
 
+def test_enter_garage_car_exists():
+    with pytest.raises(ValueError):
+        enter_garage(garage, "12345", 8)
+
+def 
