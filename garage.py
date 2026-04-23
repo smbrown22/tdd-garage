43e70ad4241d14a9ee2d71fc27c7b6d744c6cd11
smbrown22 <smbrown22@my.waketech.edu>
2026-04-23 08:46:03 -0400

    fudge capacity in fake dictionaries

--- garage.py
+++ garage.py
@@ -4,12 +4,12 @@ garage = {
 }
 
 garageOne = {
-    "capacity": 10,   # total number of spots
+    "capacity": 5,   # total number of spots
     "cars": {"40000" : 9 , "40001" : 9, "40002" : 9 , "40003" : 9 , "40004" : 9}         # car_id -> entry_hour (int)
 }
 
 garageTwo = {
-    "capacity": 10,   # total number of spots
+    "capacity": 7,   # total number of spots
     "cars": {"40000" : 9 , "40001" : 9, "40002" : 9}         # car_id -> entry_hour (int)
 }
 
