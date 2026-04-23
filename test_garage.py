e25091442f47a5354f6014c249992492794a0885
smbrown22 <smbrown22@my.waketech.edu>
2026-04-23 07:59:33 -0400

    fixed test_enter_garage_full_capacity

--- test_garage.py
+++ test_garage.py
@@ -8,4 +8,5 @@ def test_enter_garage_successful_add():
 
 def test_enter_garage_full_capacity():
     with pytest.raises(ValueError):
-        enter_garage(garage, "12345", 8)
+        enter_garage(garage, "54321", 10)
+
