25708151e7142eed7a9a25fc3766351a9f8259c3
smbrown22 <smbrown22@my.waketech.edu>
2026-04-23 07:55:35 -0400

    fixed first test

--- test_garage.py
+++ test_garage.py
@@ -3,5 +3,5 @@ from garage import garage, enter_garage
 
 def test_enter_garage_successful_add():
 
-    enter_garage(garage, car_id, entry_hour) 
+    enter_garage(garage, "12345", 8) 
     assert "12345" in garage 
